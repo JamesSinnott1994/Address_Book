0bfb8535b0cad4ea1d5a0b511c7fdfec9f2d68df
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-10 12:48:03 +0100

    [ADD] Console class added for welcome message

---
+++ src/Console.java
@@ -0,0 +1,16 @@
+public class Console {
+
+    public void presentWelcomeMessage() {
+        System.out.println();
+        System.out.println("Welcome to the Address Book program! \nPlease select an option below:");
+        System.out.println();
+
+        System.out.println("1. Add an Entry");
+        System.out.println("2. Search for an Entry");
+        System.out.println("3. Save Entries");
+        System.out.println("4. Display Entries");
+        System.out.println("5. Exit");
+    }
+
+}
+
