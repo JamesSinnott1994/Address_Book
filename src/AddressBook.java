e9e0b4bab0e0c8cf08c79dd96190f53422790453
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-10 12:59:14 +0100

    [ADD] Switch statement for options added

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,4 +4,17 @@ public class AddressBook {
 
     private List<Entry> entries;
 
+    public void addEntry() {
+
+    }
+
+    public void searchForEntry() {
+
+    }
+
+    public void saveEntries() {
+    }
+
+    public void displayEntries() {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,22 @@ public class Main {
         Console console = new Console();
 
         boolean running = true;
-
         while (running) {
-
             console.presentWelcomeMessage();
             int option = console.selectOption();
 
+            switch (option) {
+                case 1:
+                    addressBook.addEntry();
+                case 2:
+                    addressBook.searchForEntry();
+                case 3:
+                    addressBook.saveEntries();
+                case 4:
+                    addressBook.displayEntries();
+                case 5:
+                    System.exit(0);
+            }
         }
 
     }
